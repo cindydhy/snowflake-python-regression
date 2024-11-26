a71522f82852b80c05656d7ad4e46a2bc5e5ccaa
Cindy Ding <ding216@purdue.edu>
2024-11-26 10:10:04 -0800

    fixed query

--- wine-quality-scripts/initial_setup.py
+++ wine-quality-scripts/initial_setup.py
@@ -58,7 +58,7 @@ for table in table_names:
     query = "INSERT INTO " + table + " (" + column_names + ") SELECT " + column_names + " FROM WHITE_WINE_QUALITY;"
     print(query)
     cur.execute(query)
-    query = "UPDATE " + table + " SET GD_MD5_VALUE=MD5(TO_VARCHAR(ARRAY_CONSTRUCT" + column_names + "));"
+    query = "UPDATE " + table + " SET GD_MD5_VALUE=MD5(TO_VARCHAR(ARRAY_CONSTRUCT(" + column_names + ")));"
     print(query)
     cur.execute(query)
 
